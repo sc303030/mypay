8981528108e6fb4261b748b0677b63c5b439578b
sc303030 <lkh6035@naver.com>
2023-05-06 13:33:57 +0900

    23.05.06 | modify Admin Custom

--- mall/models.py
+++ mall/models.py
@@ -28,6 +28,8 @@ class Product(models.Model):
         choices=Status.choices, default=Status.INACTIVE, max_length=1
     )
     photo = models.ImageField(upload_to="mall/product/photo/%Y/%m/%d")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"<{self.pk}> {self.name}"
