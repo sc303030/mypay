9b715451d8cdba19e0802642bf5d8b4add4b41f3
sc303030 <lkh6035@naver.com>
2023-05-06 12:46:53 +0900

    23.05.06 | modify media urls

--- mypay/urls.py
+++ mypay/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -20,3 +21,5 @@ if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
