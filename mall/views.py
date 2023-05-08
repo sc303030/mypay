0c131f714c072078c304d285e08fd1ad8d141507
sc303030 <lkh6035@naver.com>
2023-05-08 10:32:27 +0900

    23.05.08 | add search box queryset

--- mall/views.py
+++ mall/views.py
@@ -9,5 +9,12 @@ class ProductListView(ListView):
     product_qs = Product.objects.all().select_related("category")
     paginate_by = 4
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get("query", "")
+        if query:
+            qs = qs.filter(name__icontains=query)
+        return qs
+
 
 product_list = ProductListView.as_view()
