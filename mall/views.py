0f11b9e0d537d88a5d0efdaa47fcbe44c9f8b685
sc303030 <lkh6035@naver.com>
2023-05-13 12:11:43 +0900

    23.05.13 | add cart javascript

--- mall/views.py
+++ mall/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.forms import modelformset_factory
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 from mall.forms import CartProductForm
@@ -52,6 +54,7 @@ def cart_detail(request):
 
 
 @login_required
+@require_POST
 def add_to_cart(request, product_pk):
     product_qs = Product.objects.filter(status=Product.Status.ACTIVE)
     product = get_object_or_404(product_qs, pk=product_pk)
@@ -63,6 +66,4 @@ def add_to_cart(request, product_pk):
         cart_product.quantity += quantity
         cart_product.save()
 
-    messages.success(request, "장바구니에 추가했습니다.")
-    redirect_url = request.META.get("HTTP_REFERER", "product_list")
-    return redirect(redirect_url)
+    return HttpResponse("ok")
